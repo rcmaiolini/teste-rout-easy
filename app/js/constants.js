55204ba7a84ec200f43d9e7e30c475c1f32691ac
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2017-09-14 21:07:00 -0300

    Removendo arquivos desnecessarios

--- app/js/constants.js
+++ app/js/constants.js
@@ -1,6 +1,5 @@
 const AppSettings = {
-  appTitle: 'Example Application',
-  apiUrl: '/api/v1'
+  
 };
 
 export default AppSettings;
--- app/js/on_run.js
+++ app/js/on_run.js
@@ -1,17 +1,7 @@
-function OnRun($rootScope, AppSettings) {
+function OnRun() {
   'ngInject';
 
-  // change page title based on state
-  $rootScope.$on('$stateChangeSuccess', (event, toState) => {
-    $rootScope.pageTitle = '';
-
-    if (toState.title) {
-      $rootScope.pageTitle += toState.title;
-      $rootScope.pageTitle += ' \u2014 ';
-    }
-
-    $rootScope.pageTitle += AppSettings.appTitle;
-  });
+  
 
 }
 
